56ba1ba26418e24d0916568110415fa0599dbea8
SebastianN97 <sebbe132132@gmail.com>
2020-09-01 13:26:44 +0200

    Creating src/Hello.jsx. Readjusting codes in index.js after Hello.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
                 test: /\.(js|jsx)$/,
                 exclude: /(node_modules)/,
                 loader: "babel-loader",
-                options: { presets: ["@@babel/env"] },
+                options: { presets: ["@babel/env"] },
             },
             {
                 test: /\.css$/,
