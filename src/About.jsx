da3f8060d3a45a9e964d5f50da97b5d38a1cbcc5
SebastianN97 <sebbe132132@gmail.com>
2020-09-03 14:05:28 +0200

    Adding unDraw to the project. Importing undrawDesignerLife to About.jsx. Importing undrawDashboard to Projects.jsx. And modifying the codes in both files.

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,22 @@
 
 import React from "react";
+import { UndrawDesignerLife } from "react-undraw-illustrations";
+import UndrawDesigner from "react-undraw-illustrations";
 
 const About = () => {
     return (
         <div className="ui main container">
-            <h1 id="about-header" className="ui header">About Me</h1>
-            <p>
-                Hello my name is Sebastian! I should write something here but don't really know yet!
-            </p>
+            <div class="ui stackable two column grid">
+                <div class="column">
+                    <UndrawDesignerLife />
+                </div>
+                <div class="column">
+                    <h1 id="about-header" className="ui header">About Me</h1>
+                    <p>
+                    Hello my name is Sebastian! I should write something here but don't really know yet!
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
