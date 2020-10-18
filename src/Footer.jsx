0843b152f0e5415372743a2daedb5dffe98a6f7e
SebastianN97 <sebbe132132@gmail.com>
2020-10-18 23:39:17 +0200

    creating buttons for linkedin and github in footer file.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,29 @@
-
 import React from "react";
 
 const Footer = () => {
-    return (
-        <footer>
-            <div className="ui container">
-                <h1 id="footer">Made with React {React.version}</h1>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="ui container">
+        <p id="footer">
+          <a
+            id="footer-url"
+            href="https://www.linkedin.com/in/sebastian-niewiadomski-8a65421b7/"
+            target="_blank"
+          >
+            <i class="linkedin large icon"></i>{" "}
+          </a>
+          | Sebastian Niewiadomski 2020 |
+          <a
+            id="footer-url"
+            href="https://github.com/SebastianN97"
+            target="_blank"
+          >
+            <i class="github large icon"></i>{" "}
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
