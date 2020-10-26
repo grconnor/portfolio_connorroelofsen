0ae5c30ca9a3a9687f5b164b67dd588453a30fba
SebastianN97 <sebbe132132@gmail.com>
2020-10-26 19:51:36 +0100

    Modifying button and fixing stucture of project component with css.

--- src/ProjectCard.jsx
+++ src/ProjectCard.jsx
@@ -3,23 +3,19 @@ import React from "react";
 const ProjectCard = ({ project }) => {
   return (
     <>
-      <div class="ui card">
+      <div id="project-card" class="ui card">
         <div class="image">
           <img src={project.image} />
         </div>
         <div class="content">
           <h3 class="ui header">{project.name}</h3>
-          <div class="description">{project.description}</div>
-
-          <a href={project.git} target="_blank">
-            <div class="ui circular animated button" id="webpage" tabindex="0">
-              <div class="hidden content">GitHub</div>
+          <div id="project-desc" class="description">{project.description}</div>
+            
+          <a id="project-github" href={project.git} target="_blank">
               <div class="visible content">
-                <i class="big grey github button"></i>{" "}
+                <i class="github large icon"></i>{" "}
               </div>
-            </div>
           </a>
-
         </div>
       </div>
     </>
