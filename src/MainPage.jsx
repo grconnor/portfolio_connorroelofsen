219b01a78afb27b84a602fe23892589adb9b2e88
Connor Roelofsen <connorroelofsen15@gmail.com>
2020-12-08 22:52:52 +0100

    Update biography for landing page.

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -11,16 +11,11 @@ const MainPage = () => {
 
         <p>
           Recently graduated from Craft Academy where I have been working in a fast 
-          paced environment, learning how to work on real life applications and situations. <br></br>
-          I got solid knowledge on working with javascript, react and react native for the front end 
-          and ruby on rails for the back end. <br></br>
-          Working with a test driven development approach, since I believe it makes the code 
-          easier to maintain and understand in the long term. <br></br>
-          I enjoy being challenged with new projects that require me to work outside my comfort zone, 
-          as continuing to learn new languages and development techniques are important 
-          for me to grow as a developer. 
-          It is said about me that my specialties include leadership, teamwork, having the ability to 
-          lift others with positive reinforcement and generally improving the working environment in a positive way. 
+          paced environment, working on real life applications and situations. <br></br>
+          Following a TDD and Agile approach while developing as it sets the foundation for the application at hand.<br></br>
+          Love meeting new people, passing on knowledgelearning new techknowlogies, perfecting my technological way of thinking and working.<br></br>
+          It is often said about me that I'm driven, communicative, and skilled in problem solving and research.
+
         </p>
 
         <h1>
@@ -38,11 +33,3 @@ const MainPage = () => {
 };
 
 export default MainPage;
-
-{/* <div class="ui stackable two column grid">
-  <div class="column">
-    <img
-      id="profile-img"
-      src="https://scontent.fgse1-1.fna.fbcdn.net/v/t1.0-9/41841661_1784413611607957_6140001739312463872_o.jpg?_nc_cat=100&ccb=2&_nc_sid=09cbfe&_nc_ohc=nqUJ4-WLKZIAX9W2sVK&_nc_ht=scontent.fgse1-1.fna&oh=6af63e8bdbf21b3baa221d5abd05d156&oe=5FE1A2BE"
-    />
-  </div> */}
