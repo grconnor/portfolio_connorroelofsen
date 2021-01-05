52e2442c9b71092942118024c6445a71d260d56f
Connor Roelofsen <connorroelofsen15@gmail.com>
2021-01-05 20:01:13 +0100

    Clean CVDownload component.

--- src/CVDownload.jsx
+++ src/CVDownload.jsx
@@ -3,21 +3,9 @@ import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import mycvPDF from "./mycv.pdf";
 
 const CVDownload = () => {
-  const [numPages, setNumPages] = useState(1);
-  const [pageNumber, setPageNumber] = useState(1);
-
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
-
   return (
     <div className="cv-pdf">
-      <Document file={mycvPDF} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
-      </Document>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
+      <Document file={mycvPDF} onLoadSuccess={onDocumentLoadSuccess}></Document>
     </div>
   );
 };
